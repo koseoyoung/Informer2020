ac37d105c6c683e570f5a94d24940cdfcbe68e6d
koseoyoung <dorothy0602@gmail.com>
2023-04-04 20:28:53 -0400

    feat: modify pcap-converter script

--- pcap-converter.py
+++ pcap-converter.py
@@ -10,6 +10,9 @@ with open('raw.csv', 'r') as file:
 time_index = rows[0].index('time')
 initial_index = rows[0].index('srcip')
 proto_index = rows[0].index('proto')
+offset_index = rows[0].index('off')
+ihl_index = rows[0].index('ihl')
+version_index = rows[0].index('version')
 
 for i, row in enumerate(rows):
     converted = ''
@@ -21,9 +24,13 @@ for i, row in enumerate(rows):
         row[proto_index] = 0
         
     row[time_index], row[initial_index] =row[initial_index], converted
-
+    
+    del row[offset_index] # delete all same values coloumn or string instead of integer values 
+    del row[ihl_index]
+    del row[version_index]
+    del row[proto_index]
 
 # Write the updated rows to a new CSV file
-with open('updated_raw.csv', 'w', newline='') as file:
+with open('updated_pcap.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(rows)
