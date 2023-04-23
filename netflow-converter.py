81cca77642aa98d0ad7eae808538d1997f2bc480
koseoyoung <dorothy0602@gmail.com>
2023-04-22 22:44:36 -0400

    add urg converter script and updated csv files

--- netflow-converter.py
+++ netflow-converter.py
@@ -3,16 +3,14 @@ from datetime import datetime
 
 
 # Open the CSV file
-with open('caida-raw.csv', 'r') as file:
+with open('urg16-raw.csv', 'r') as file:
     reader = csv.reader(file)
     rows = list(reader)
 
-time_index = rows[0].index('time')
+time_index = rows[0].index('ts')
 initial_index = rows[0].index('srcip')
+type_index = rows[0].index('type')
 proto_index = rows[0].index('proto')
-offset_index = rows[0].index('off')
-ihl_index = rows[0].index('ihl')
-version_index = rows[0].index('version')
 
 count = 0
 
@@ -24,20 +22,18 @@ for i, row in enumerate(rows):
     if i == 0: 
         converted = 'date'
     else: 
-        unix_timestamp = int(row[time_index]) / 1000000
+        unix_timestamp = int(float(row[time_index]) / 1000000)
         converted = datetime.utcfromtimestamp(unix_timestamp).strftime('%Y-%m-%d %H:%M:%S')
         row[proto_index] = 0
         
     row[time_index], row[initial_index] =row[initial_index], converted
     
-    del row[offset_index] # delete all same values coloumn or string instead of integer values 
-    del row[ihl_index]
-    del row[version_index]
-    del row[proto_index]
-    
+    del row[type_index] # delete all same values coloumn or string instead of integer values 
+    del row[proto_index] # delete all same values coloumn or string instead of integer values 
+
 print("count: ", count)
 
 # Write the updated rows to a new CSV file
-with open('updated_pcap.csv', 'w', newline='') as file:
+with open('updated_urg16.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(rows)
